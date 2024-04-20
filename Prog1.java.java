869c22eb813fcb812e184b73c14b2f9a18b1b668
nel sanico <ronelsanico19@gmail.com>
2024-04-20 15:23:41 +0800

    merging fix

--- Prog1.java.java
+++ Prog1.java.java
@@ -1,5 +1,6 @@
 public class Prog1 {
     public static void main (string[] args) {
+        System.out.println("PATEROS TECHNOLOGICAL COLLEGE");
         System.out.println("PATEROS");
         System.out.println("TECHNOLOGICAL");
         System.out.println("COLLEGE");
